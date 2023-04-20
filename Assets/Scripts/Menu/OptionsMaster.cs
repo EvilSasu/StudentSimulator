cf056663b2caab962f88dcde55737528bd5d8bbe
Daniel Ostrowski <daniel.ostrowski2703@o2.pl>
2023-04-20 13:53:00 +0200

    Added options

---
+++ Assets/Scripts/Menu/OptionsMaster.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMaster : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> resolutionOptions = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if (resolutions[i].width >= 800)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+
+                resolutionOptions.Add(option);
+
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }       
+        }
+
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        //Debug.Log(resolutionDropdown.value);
+        //Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(int isFullscreen)
+    {
+        if (isFullscreen == 0) Screen.fullScreen = true;
+        else Screen.fullScreen = false;
+        Debug.Log(Screen.fullScreenMode);
+    }
+}
