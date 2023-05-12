8eaa732bae5b19d5ac8f1b49adf6ecc0b3cd63e1
LoveNotWar <s089011@student.tu.kielce.pl>
2023-05-12 22:55:51 +0200

    Update Door.cs

--- Assets/Minigames/Shop/Scripts/Other/Door.cs
+++ Assets/Minigames/Shop/Scripts/Other/Door.cs
@@ -8,7 +8,7 @@ public class Door : MonoBehaviour
     public bool Key;
     public bool rKey, gKey, bKey;
 
-
+    public LevelLoaderScript loader;
     public GameObject spawn;
     //public GameObject door;
 
@@ -22,13 +22,15 @@ public class Door : MonoBehaviour
                 //wymagany klucz
                 if (rKey && other.GetComponent<PlayerInventory>().rKey && gKey && other.GetComponent<PlayerInventory>().gKey && bKey && other.GetComponent<PlayerInventory>().bKey)
                 {
+                    loader.LoadChoosenLevel(4);
+
 
                     //spawn.SetActive(true);
                     //nastêpny poziom
                     //Scene sceneToLoad = SceneManager.GetSceneByName("ztestWorld");
                     //SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Additive);
                     //SceneManager.MoveGameObjectToScene(transform.gameObject, sceneToLoad);
-                      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //SceneManager.GetActiveScene().buildIndex + 1
+                    //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //SceneManager.GetActiveScene().buildIndex + 1
                 }
             }
 
