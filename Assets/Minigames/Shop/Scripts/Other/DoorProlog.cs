40d27b330a5d78d0303bcc407c3fc4a92046a7db
LoveNotWar <s089011@student.tu.kielce.pl>
2023-05-12 22:51:07 +0200

    zaraz mnie pacnie

---
+++ Assets/Minigames/Shop/Scripts/Other/DoorProlog.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorProlog : MonoBehaviour
+{
+    public bool Key;
+    public bool rKey, gKey, bKey;
+
+    public LevelLoaderScript loader;
+    public GameObject spawn;
+    //public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            if (Key)
+            {
+                //wymagany klucz
+                if (rKey && other.GetComponent<PlayerInventory>().rKey && gKey && other.GetComponent<PlayerInventory>().gKey && bKey && other.GetComponent<PlayerInventory>().bKey)
+                {
+                    loader.LoadChoosenLevel(4);
+
+
+                    //spawn.SetActive(true);
+                    //nastêpny poziom
+                    //Scene sceneToLoad = SceneManager.GetSceneByName("ztestWorld");
+                    //SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Additive);
+                    //SceneManager.MoveGameObjectToScene(transform.gameObject, sceneToLoad);
+                    // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //SceneManager.GetActiveScene().buildIndex + 1
+                }
+            }
+
+        }
+
+    }
+}
