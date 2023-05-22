286ef2bd5d5ae245e0033c7560eccc6116202539
Daniel Ostrowski <daniel.ostrowski2703@o2.pl>
2023-05-22 17:00:36 +0200

    s

--- Assets/Scripts/Gameplay/EventSystem/UczelniaRandomEvents.cs
+++ Assets/Scripts/Gameplay/EventSystem/UczelniaRandomEvents.cs
@@ -8,21 +8,51 @@ public class UczelniaRandomEvents : MonoBehaviour
     public List<StoryScene> listOfKolekScenes;
     public List<StoryScene> listOfRobotScenes;
     public List<StoryScene> listOfZarowaScenes;
+    public List<StoryScene> listOfGargScenes;
 
     private bool waitEnding = false;
+    private bool eventStarted = false;
     private PlayerData player;
     private GameData gameData;
+    private BackgroundController gameMaster;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("PlayerData").GetComponent<PlayerData>();
         gameData = GameObject.FindGameObjectWithTag("GameData").GetComponent<GameData>();
-        StartCoroutine(StartWait());
+        gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<BackgroundController>();
+        if (!waitEnding)
+            StartCoroutine(StartWait());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (waitEnding == true && eventStarted == false)
+        {
+            ChooseListOfRandomEvents();
+        }
+    }
+
+    private void ChooseListOfRandomEvents()
+    {
+        eventStarted = true;
+
+        if (gameData.dayOfWeek == 0)
+            ChooseEvent(listOfGruszkaScenes);
+        if (gameData.dayOfWeek == 1)
+            ChooseEvent(listOfKolekScenes);
+        if (gameData.dayOfWeek == 2)
+            ChooseEvent(listOfRobotScenes);
+        if (gameData.dayOfWeek == 3)
+            ChooseEvent(listOfZarowaScenes);
+        if (gameData.dayOfWeek == 4)
+            ChooseEvent(listOfGargScenes);
+    }
+
+    private void ChooseEvent(List<StoryScene> list)
+    {
+        int choice = Random.Range(0, list.Count);
+        gameMaster.firstScene = list[choice];
+        gameMaster.PlayFirstDialogue();
     }
 
     IEnumerator StartWait()
