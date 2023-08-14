91e310de8abf2716268ea4bd0a278c204fd69da9
Daniel Ostrowski <daniel.ostrowski2703@o2.pl>
2023-08-14 13:56:57 +0200

    PopUp done

---
+++ Assets/Scripts/Gameplay/PopUpSystem/PopUp.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+   // bool hasAnimationStarted = false;
+    public int amountOfPopUp = 0;
+
+    private void Awake()
+    {
+        StartCoroutine(AnimatePopUp(amountOfPopUp));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StarAnimation(int amountOfPopUp)
+    {
+        StartCoroutine(AnimatePopUp(amountOfPopUp));
+    }
+
+    private IEnumerator AnimatePopUp(int amountOfPopUp)
+    {
+        float posToMove = 500f - 75f - ((amountOfPopUp - 1) * 50f);
+        while (this.transform.localPosition.y <= posToMove)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, 
+                new Vector2(this.transform.position.x, posToMove), 0.1f);
+            yield return new WaitForSeconds(.01f);
+        }
+            
+        yield return new WaitForSeconds(2f);
+        this.GetComponent<Animator>().SetTrigger("Hide");
+        yield return new WaitForSeconds(2f);
+        Debug.Log("jestem");
+        GameObject.Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Gameplay/PopUpSystem/PopUpInfoMeneger.cs
+++ Assets/Scripts/Gameplay/PopUpSystem/PopUpInfoMeneger.cs
@@ -6,29 +6,20 @@ using TMPro;
 public class PopUpInfoMeneger : MonoBehaviour
 {
     public GameObject PopUp;
-    private List<GameObject> popUpList;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private List<GameObject> popUpList = new List<GameObject>();
 
     public void CreatePopUp(string name, int value, bool isIncreased)
     {
-        if(value >= 1)
+        if (value >= 1)
         {
             string textOnPopUP;
             GameObject newPopUP = Instantiate(PopUp, new Vector3(0f, 0f, 0f), Quaternion.identity);
-            newPopUP.SetActive(true);
             
             newPopUP.transform.SetParent(this.transform);
             newPopUP.transform.localScale = new Vector3(1f, 1f, 1f);
-            //popUpList.Add(newPopUP);
+            popUpList.Add(newPopUP);
+            newPopUP.GetComponent<PopUp>().amountOfPopUp = popUpList.Count;
+            newPopUP.SetActive(true);
 
             if (isIncreased)
                 textOnPopUP = new string(name + " has increased by " + value);
@@ -36,15 +27,6 @@ public class PopUpInfoMeneger : MonoBehaviour
                 textOnPopUP = new string(name + " has decreased by " + value);
 
             newPopUP.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = textOnPopUP;
-
-            //StartCoroutine(AnimatePopUp(newPopUP));
         }       
-    }
-
-    private IEnumerator AnimatePopUp(GameObject newPopUP)
-    {
-        newPopUP.transform.position = Vector2.MoveTowards(newPopUP.transform.position, new Vector2(newPopUP.transform.position.x, 1800f), 0.5f);
-        yield return new WaitForSeconds(2f);
-        newPopUP.GetComponent<Animator>().SetTrigger("Hide");
-    }
+    }   
 }
