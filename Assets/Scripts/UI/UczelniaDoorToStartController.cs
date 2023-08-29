11b0c173ea2f89ed516e6d193860adb82860d93c
Daniel Ostrowski <daniel.ostrowski2703@o2.pl>
2023-08-30 00:26:01 +0200

    s

--- Assets/Scripts/UI/UczelniaDoorToStartController.cs
+++ Assets/Scripts/UI/UczelniaDoorToStartController.cs
@@ -11,7 +11,7 @@ public class UczelniaDoorToStartController : MonoBehaviour
 
     public void UseDoor()
     {
-        if (gameData.hour >= 8 && gameData.hour <= 16)
+        if ((gameData.hour >= 8 && gameData.hour <= 16) && (gameData.dayOfWeek >= 0 && gameData.dayOfWeek <= 4))
         {
             if(player.energy >= 20)
                 loader.LoadChoosenLevel(10);
@@ -19,6 +19,6 @@ public class UczelniaDoorToStartController : MonoBehaviour
                 pop.CreateSpecialPopUp("Jesteœ zbyt zmêczony!");
         }
         else
-            pop.CreateSpecialPopUp("Zajêcia odbywaj¹ siê 8:00 - 16:00");
+            pop.CreateSpecialPopUp("Zajêcia odbywaj¹ siê 8:00 - 16:00 w Poniedzia³ek - pi¹tek");
     }
 }
